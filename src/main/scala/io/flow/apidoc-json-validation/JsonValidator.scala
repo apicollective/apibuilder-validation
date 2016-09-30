efdbabe9df7767ef87bf690f7b5822e606803444
Michael Bryzek <mbryzek@alum.mit.edu>
2016-09-30 10:52:12 -0400

    Add enum validation

--- src/main/scala/io/flow/apidoc-json-validation/JsonValidator.scala
+++ src/main/scala/io/flow/apidoc-json-validation/JsonValidator.scala
@@ -32,7 +32,7 @@ case class JsonValidator(val service: Service) {
   ): Either[Seq[String], JsValue] = {
     service.enums.find(_.name == typeName) match {
       case Some(e) => {
-        validateEnum(e, js, prefix)
+        validateEnum(prefix.getOrElse("Body"), e, js)
       }
 
       case None => {
@@ -82,14 +82,26 @@ case class JsonValidator(val service: Service) {
   }
 
   private[this] def validateEnum(
+    prefix: String,
     enum: Enum,
-    js: JsValue,
-    prefix: Option[String]
+    js: JsValue
   ): Either[Seq[String], JsValue] = {
-    validateString(prefix.getOrElse(s"Enum '${enum.name}'"), js) match {
+    validateString(prefix, js) match {
       case Left(errors) => Left(errors)
-      case Right(value) => {
-        Right(value)
+      case Right(jsString) => {
+        val incomingValue = jsString.value.trim
+        val valid = enum.values.map(_.name)
+        valid.find { _.toLowerCase.trim == incomingValue.toLowerCase } match {
+          case None => {
+            Left(
+              Seq(
+                s"$prefix invalid value '${incomingValue}'. Valid values for the enum '${enum.name}' are: " +
+                  valid.mkString("'", "', '", "'")
+              )
+            )
+          }
+          case Some(validValue) => Right(JsString(validValue))
+        }
       }
     }
   }
