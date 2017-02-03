8bc6c220dbf2eac4443a6b15f69809ed19f1b981
Michael Bryzek <mbryzek@alum.mit.edu>
2017-02-03 15:44:33 -0500

    Fix infinite loop w/ union type validation (#36)
    * Revert "Disable union type validation until we diagnose infinite loop"
    
    This reverts commit 2ecf95f21a0c83db174930f19b7c08b2c4b3ced7.
    
    * Fix infinite loop w/ union type validation
    
    * Add positive test
    
    * Update test w/ example that demonstrates past infinite loop
    
    * Add assertion to prevent infinite loop

--- src/main/scala/io/flow/apidoc-json-validation/JsonValidator.scala
+++ src/main/scala/io/flow/apidoc-json-validation/JsonValidator.scala
@@ -161,17 +161,24 @@ case class JsonValidator(val service: Service) {
   ): Either[Seq[String], JsValue] = {
     union.discriminator match {
       case None => {
-        // Skip validation; not a use case at flow as there is no
-        // discriminator specified
+        // TODO: if we want to validate, we would expect js object to have one key
+        // that is the name of the type.
         Right(js)
       }
 
       case Some(discriminator) => {
         (js \ discriminator).asOpt[String] match {
-          case None => Left(Seq(s"Union type '${union.name}' requires a field named '${discriminator}'"))
+          case None => Left(Seq(s"Union type '${union.name}' requires a field named '$discriminator'"))
           case Some(value) => {
-            // validate(value, js, prefix)
-            Right(js)
+            union.types.find(_.`type` == value) match {
+              case None => {
+                Left(Seq(s"Invalid discriminator '$value' for union type '${union.name}': must be one of " + union.types.map(_.`type`).mkString("'", "', '", "'")))
+              }
+              case Some(_) => {
+                assert(value != union.name, s"Specific union type name cannot match name of the union itself - else we'd recurse infinitely")
+                validate(value, js, prefix)
+              }
+            }
           }
         }
       }
