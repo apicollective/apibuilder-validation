cb242ddc03d6a943167672b9879d54cb478e0048
Michael Bryzek <mike@flow.io>
2019-06-14 12:53:23 -0400

    wip

---
+++ src/main/scala/io/apibuilder/validation/ServiceResolver.scala
@@ -0,0 +1,48 @@
+package io.apibuilder.validation
+
+import java.util.concurrent.ConcurrentHashMap
+
+import io.apibuilder.spec.v0.models._
+
+case class ServiceResolver(services: List[ApiBuilderService]) {
+
+  private[this] val cache = new ConcurrentHashMap[String, Option[ApiBuilderService]]()
+
+  /**
+    * resolve the API Builder service defined at the provided method, path.
+    * if no service, return a nice error message. Otherwise invoke
+    * the provided function on the API Builder service.
+    */
+  def resolve(method: Method, path: String): Option[ApiBuilderService] = {
+    cache.computeIfAbsent(
+      s"$method$path",
+      _ => { doResolveService(method, path) }
+    )
+  }
+
+  private[this] def doResolveService(method: Method, path: String): Option[ApiBuilderService] = {
+    services.filter { s =>
+      s.isDefinedAt(method = method, path = path)
+    } match {
+      case Nil => None
+      case one :: Nil => Some(one)
+      case multiple => {
+        // If we find a static path in any service, return that one.
+        // Otherwise return the first matching service. This handles ambiguity:
+        //   - service 1 defines POST /:organization/tokens
+        //   - service 2 defines POST /users/tokens
+        // We want to return service 2 when the path is /users/tokens
+        multiple.find { s =>
+          s.validate(method, path) match {
+            case Right(op) if Route.isStatic(op.path) => true
+            case _ => false
+          }
+        }.orElse {
+          multiple.headOption
+        }
+      }
+    }
+  }
+}
+
+
