50ad0190ceb2b772da78c1211c36588ce7b50cd5
Michael Bryzek <mike@flow.io>
2019-06-14 22:59:39 -0400

    Avoid having to evaluate all routes by stopping at first

--- src/main/scala/io/apibuilder/validation/ServiceOperationResolver.scala
+++ src/main/scala/io/apibuilder/validation/ServiceOperationResolver.scala
@@ -2,13 +2,18 @@ package io.apibuilder.validation
 
 import io.apibuilder.spec.v0.models._
 
-private[validation] case class ServiceOperationCache(services: List[ApiBuilderService]) {
-
+private[validation] case class ServiceOperationByTypeCache(
+  services: List[ApiBuilderService]
+)(
+  acceptPath: String => Boolean
+) {
   private[this] case class Entry(route: Route, operation: ApiBuilderOperation)
 
   private[this] val entries: List[Entry] = {
     services.flatMap { s =>
-      s.service.resources.flatMap(_.operations).map { op =>
+      s.service.resources.flatMap(_.operations)
+        .filter { op => acceptPath(op.path) }
+        .map { op =>
         Entry(
           Route(op.method, op.path),
           ApiBuilderOperation(s, op)
@@ -23,8 +28,27 @@ private[validation] case class ServiceOperationCache(services: List[ApiBuilderSe
 
   private[this] def numberSlashes(path: String): Int = path.count(_ == '/')
 
-  def find(method: Method, path: String): List[ApiBuilderOperation] = {
-    entriesByNumberSlashes.getOrElse(numberSlashes(path), List.empty).filter(_.route.matches(method, path)).map(_.operation)
+  def find(method: Method, path: String): Option[ApiBuilderOperation] = {
+    entriesByNumberSlashes.getOrElse(numberSlashes(path), List.empty)
+      .find(_.route.matches(method, path))
+      .map(_.operation)
+  }
+}
+
+private[validation] case class ServiceOperationCache(services: List[ApiBuilderService]) {
+
+  private[this] val static = ServiceOperationByTypeCache(services)(Route.isStatic)
+  private[this] val dynamic = ServiceOperationByTypeCache(services)(Route.isDynamic)
+
+  // If we find a static path in any service, return that one.
+  // Otherwise return the first matching service. This handles ambiguity:
+  //   - service 1 defines POST /:organization/tokens
+  //   - service 2 defines POST /users/tokens
+  // We want to return service 2 when the path is /users/tokens
+  def find(method: Method, path: String): Option[ApiBuilderOperation] = {
+    static.find(method, path).orElse {
+      dynamic.find(method, path)
+    }
   }
 }
 
@@ -33,27 +57,11 @@ case class ServiceOperationResolver(services: List[ApiBuilderService]) {
   private[this] val cache = ServiceOperationCache(services)
 
   /**
-    * resolve the API Builder service defined at the provided method, path.
-    * if no service, return a nice error message. Otherwise invoke
-    * the provided function on the API Builder service.
+    * resolve the API Builder service & operation defined at the provided
+    * method, path.
     */
   def findOperation(method: Method, path: String): Option[ApiBuilderOperation] = {
-    cache.find(method, path) match {
-      case Nil => None
-      case one :: Nil => Some(one)
-      case multiple => {
-        // If we find a static path in any service, return that one.
-        // Otherwise return the first matching service. This handles ambiguity:
-        //   - service 1 defines POST /:organization/tokens
-        //   - service 2 defines POST /users/tokens
-        // We want to return service 2 when the path is /users/tokens
-        multiple.find { apiBuilderOperation =>
-          Route.isStatic(apiBuilderOperation.operation.path)
-        }.orElse {
-          multiple.headOption
-        }
-      }
-    }
+    cache.find(method, path)
   }
 }
 
