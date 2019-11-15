ab80515299018d4005f4fe0bc9855e7ff1dfbe14
Michael Bryzek <mike@flow.io>
2019-11-14 16:10:33 -0800

    wip

--- src/main/scala/io/apibuilder/validation/MultiServiceImpl.scala
+++ src/main/scala/io/apibuilder/validation/MultiServiceImpl.scala
@@ -1,7 +1,8 @@
 package io.apibuilder.validation
 
+import io.apibuilder.spec.v0.models
 import io.apibuilder.spec.v0.models.Method
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsObject, JsValue, Json}
 
 /**
   * Wrapper to work with multiple API Builder services.
@@ -24,7 +25,43 @@ case class MultiServiceImpl(
   }
 
   override def upcast(typ: ApiBuilderType, js: JsValue): Either[Seq[String], JsValue] = {
-    validator.validateType(typ, js)
+    val finalJs = typ match {
+      case m: ApiBuilderType.Model => {
+        js match {
+          case j: JsObject => injectModelsWithOptionalFields(m, j)
+          case _ => js
+        }
+      }
+      case _: ApiBuilderType.Enum |  _: ApiBuilderType.Union => js
+    }
+    validator.validateType(typ, finalJs)
   }
 
+
+  private[this] def injectModelsWithOptionalFields(typ: ApiBuilderType.Model, js: JsObject): JsObject = {
+    typ.model.fields.filter(_.required).filter { f =>
+      (js \ f.name).toOption.isEmpty
+    }.foldLeft(js) { case (js, field) =>
+      println(s"field: ${field.name} type[${field.`type`}]")
+      if (isFieldModelWithAllRequiredFields(typ.service, field.`type`)) {
+        js ++ Json.obj(field.name -> Json.obj())
+      } else {
+        js
+      }
+    }
+  }
+
+  private[this] def isFieldModelWithAllRequiredFields(service: models.Service, typ: String): Boolean = {
+    println(s"type: ${findType(service.namespace, typ)}")
+    findType(service.namespace, typ) match {
+      case None => false
+      case Some(t) => t match {
+        case m: ApiBuilderType.Model => {
+          println(s"model: ${m.name}")
+          true
+        }
+        case _: ApiBuilderType.Enum | _: ApiBuilderType.Union => false
+      }
+    }
+  }
 }
