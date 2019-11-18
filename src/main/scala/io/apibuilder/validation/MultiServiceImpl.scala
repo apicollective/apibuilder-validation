9b171ddddf770919429f1ee5d142a327c13b38c7
Michael Bryzek <mike@flow.io>
2019-11-18 14:54:22 -0500

    Remove model defaults

--- src/main/scala/io/apibuilder/validation/MultiServiceImpl.scala
+++ src/main/scala/io/apibuilder/validation/MultiServiceImpl.scala
@@ -25,50 +25,7 @@ case class MultiServiceImpl(
   }
 
   override def upcast(typ: ApiBuilderType, js: JsValue): Either[Seq[String], JsValue] = {
-    val finalJs = (typ, js) match {
-      case (m: ApiBuilderType.Model, j: JsObject) => {
-        val updated = createDefault(m).deepMerge(j)
-        if (js != updated && validator.validateType(m, updated).isRight) {
-          // Note we only merge here if the merged object itself is valid.
-          // This is important as it ensures that any validation messages
-          // are clear (highest node) and that we don't inject a default
-          // for a nested optional object that does not validate
-          updated
-        } else {
-          js
-        }
-      }
-      case _ => js
-    }
-    validator.validateType(typ, finalJs)
+    validator.validateType(typ, js)
   }
 
-  /**
-   * Creates a json object representing the default values
-   * for any models with optional fields.
-   */
-  private[this] def createDefault(typ: ApiBuilderType.Model): JsObject = {
-    typ.model.fields.foldLeft(Json.obj()) { case (js, f) =>
-      createDefault(typ.service, f.`type`) match {
-        case None => js
-        case Some(defaults) => {
-          if (defaults.keys.isEmpty && !f.required) {
-            // if defaults is an empty object and is optional,
-            // do not include as including it may trigger other
-            // validation
-            js
-          } else {
-            js ++ Json.obj(f.name -> defaults)
-          }
-        }
-      }
-    }
-  }
-
-  private[this] def createDefault(service: models.Service, typ: String): Option[JsObject] = {
-    findType(service.namespace, typ).flatMap {
-      case m: ApiBuilderType.Model => Some(createDefault(m))
-      case _: ApiBuilderType.Enum | _: ApiBuilderType.Union => None
-    }
-  }
 }
