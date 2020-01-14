42f6f1fc659c0e8ae5c0c33578b07d448bc7f4e7
Gregor Heine <gregor@flow.io>
2020-01-14 15:11:34 +0000

    Add cats Validated UrlDownloader

---
+++ src/test/scala/io/apibuilder/validation/util/ValidatedUrlDownloaderSpec.scala
@@ -0,0 +1,37 @@
+package io.apibuilder.validation.util
+
+import cats.data.NonEmptyChain
+import cats.data.Validated.{Invalid, Valid}
+import cats.implicits._
+import io.apibuilder.validation.helpers
+import org.scalatest.{FunSpec, Matchers}
+
+class ValidatedValidatedUrlDownloaderSpec extends FunSpec with Matchers with helpers.FileHelpers {
+  describe("java.net.URL") {
+    it("handles the happy path") {
+      val urlStr = writeToTempFile("").toURI.toURL
+      ValidatedUrlDownloader.withInputStream(urlStr)(_ => "yeah".validNec) shouldBe Valid("yeah")
+    }
+
+    it("handles the error case") {
+      val urlStr = writeToTempFile("").toURI.toURL
+      ValidatedUrlDownloader.withInputStream(urlStr)(_ => "boo".invalidNec) shouldBe Invalid(NonEmptyChain.one("boo"))
+    }
+  }
+
+  describe("Url String") {
+    it("handles the happy path") {
+      val urlStr = writeToTempFile("").toURI.toURL.toString
+      ValidatedUrlDownloader.withInputStream(urlStr)(_ => "yeah".validNec) shouldBe Valid("yeah")
+    }
+
+    it("handles the invalid URL case") {
+      ValidatedUrlDownloader.withInputStream("boom")(_ => "unused".validNec) shouldBe Invalid(NonEmptyChain.one("Invalid URL: no protocol: boom"))
+    }
+
+    it("handles the error case") {
+      val urlStr = writeToTempFile("").toURI.toURL.toString
+      ValidatedUrlDownloader.withInputStream(urlStr)(_ => "boo".invalidNec) shouldBe Invalid(NonEmptyChain.one("boo"))
+    }
+  }
+}
