a3e8795864e5996c581bd678cdfb734402a33e18
Michael Bryzek <mbryzek@alum.mit.edu>
2024-07-10 09:35:51 -0400

    wip

---
+++ src/test/scala/io/apibuilder/validation/util/UrlDownloaderSpec.scala
@@ -0,0 +1,62 @@
+package io.apibuilder.validation.util
+
+import cats.implicits.*
+import io.apibuilder.helpers.TestHelpers
+import io.apibuilder.validation.helpers
+import io.apibuilder.validation.helpers.FileHelpers
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class UrlDownloaderSpec extends AnyWordSpec with Matchers with FileHelpers with TestHelpers {
+  "java.net.URL" must {
+    "handles the happy path" in {
+      val urlStr = writeToTempFile("").toURI.toURL
+      expectValid {
+        UrlDownloader.withInputStream(urlStr)(_ => ().validNec)
+      }
+    }
+
+    "handles the error case" in {
+      val urlStr = writeToTempFile("").toURI.toURL
+      expectInvalidNec {
+        UrlDownloader.withInputStream(urlStr)(_ => "".invalidNec)
+      }
+    }
+
+    "handles the empty error case" in {
+      val urlStr = writeToTempFile("").toURI.toURL
+      expectInvalidNec {
+        UrlDownloader.withInputStream(urlStr)(_ => "".invalidNec)
+      }
+    }
+  }
+
+  "Url String" must {
+    "handles the happy path" in {
+      val urlStr = writeToTempFile("").toURI.toURL.toString
+      expectValidNec {
+        UrlDownloader.withInputStream(urlStr)(_ => ().validNec)
+      }
+    }
+
+    "handles the invalid URL case" in {
+      expectInvalidNec {
+        UrlDownloader.withInputStream("boom")(_ => ().validNec)
+      } mustBe Seq("Invalid URL: no protocol: boom")
+    }
+
+    "handles the error case" in {
+      val urlStr = writeToTempFile("").toURI.toURL.toString
+      expectInvalidNec {
+        UrlDownloader.withInputStream(urlStr)(_ => "".invalidNec)
+      }
+    }
+
+    "handles the empty error case" in {
+      val urlStr = writeToTempFile("").toURI.toURL.toString
+      expectInvalidNec {
+        UrlDownloader.withInputStream(urlStr)(_ => "".invalidNec)
+      }
+    }
+  }
+}
