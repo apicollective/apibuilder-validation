a7a36f331d9f40b52bafaa9f1ed09068992f9ae7
Michael Bryzek <mbryzek@alum.mit.edu>
2024-07-10 17:25:53 -0400

    wip

--- src/test/scala/io/apibuilder/validation/MultiServiceImplSpec.scala
+++ src/test/scala/io/apibuilder/validation/MultiServiceImplSpec.scala
@@ -1,55 +1,86 @@
 package io.apibuilder.validation
 
+import io.apibuilder.builders.ApiBuilderServiceBuilders
+import io.apibuilder.helpers.{Helpers, TestHelpers}
 import io.apibuilder.spec.v0.models.Method
-import io.apibuilder.validation.helpers.Helpers
-import play.api.libs.json._
-import org.scalatest.matchers.should.Matchers
+import play.api.libs.json.*
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class MultiServiceImplSpec extends AnyFunSpec with Matchers with Helpers {
+class MultiServiceImplSpec extends AnyWordSpec with Matchers with Helpers with TestHelpers with ApiBuilderServiceBuilders {
 
   private lazy val multi = MultiServiceImpl(
-    List("flow-api-service.json", "apibuilder-api-service.json").map(loadService)
+    List(
+      ApiBuilderService(makeService(
+        name = "API",
+        models = Seq(
+          makeModel("user"),
+          makeModel("user_form"),
+          makeModel("organization"),
+          makeModel("webhook"),
+          makeModel("webhook_form"),
+        ),
+        resources = Seq(
+          makeResource("user", operations = Seq(
+            makeOperation(Method.Get, "/users", parameters = Seq(makeParameter("id"))),
+            makeOperation(Method.Post, "/users", body = Some(makeBody("user_form"))),
+          )),
+          makeResource("webhook", operations = Seq(
+            makeOperation(Method.Post, "/:organization/webhooks", body = Some(makeBody("webhook_form")))
+          ))
+        ))
+      ),
+      ApiBuilderService(makeService(
+        name = "Spec",
+        models = Seq(
+          makeModel("application"),
+          makeModel("application_form"),
+        ),
+        resources = Seq(
+          makeResource("application", operations = Seq(
+            makeOperation(Method.Post, "/:orgKey", body = Some(makeBody("application_form")))
+          ))
+        )
+      )),
+    )
   )
 
-  it("loads multiple services") {
-    multi.services.map(_.service.name) should equal(Seq("API", "apibuilder api"))
+  "loads multiple services" in {
+    multi.services.map(_.service.name) mustBe Seq("API", "Spec")
   }
 
-  it("bodyTypeFromPath") {
-    multi.bodyTypeFromPath("POST", "/unknown/path/that/does/not/resolve") should equal(None)
+  "bodyTypeFromPath" in {
+    multi.bodyTypeFromPath("POST", "/unknown/path/that/does/not/resolve") must equal(None)
 
     // resources from flow api
-    multi.bodyTypeFromPath("POST", "/users").map(_.name) should equal(Some("user_form"))
-    multi.bodyTypeFromPath("POST", "/:organization/webhooks").map(_.name) should equal(Some("webhook_form"))
+    multi.bodyTypeFromPath("POST", "/users").map(_.name) must equal(Some("user_form"))
+    multi.bodyTypeFromPath("POST", "/:organization/webhooks").map(_.name) must equal(Some("webhook_form"))
 
     // resources from apidoc api
-    multi.bodyTypeFromPath("POST", "/:orgKey").map(_.name) should equal(Some("application_form"))
+    multi.bodyTypeFromPath("POST", "/:orgKey").map(_.name) must equal(Some("application_form"))
   }
 
-  it("operation") {
-    multi.findOperation(Method.Post, "/foo/bar/baz") should be(None)
+  "operation" in {
+    multi.findOperation(Method.Post, "/foo/bar/baz") must be(None)
 
     val op = multi.findOperation("POST", "/users").get.operation
-    op.method should equal(Method.Post)
-    op.path should equal("/users")
-    op.parameters should be(Nil)
+    op.method must equal(Method.Post)
+    op.path must equal("/users")
+    op.parameters must be(Nil)
 
-    multi.findOperation("GET", "/users").get.operation.parameters.map(_.name) should be(
-      Seq("id", "email", "status", "limit", "offset", "sort")
-    )
-    multi.findOperation("GET", "/org/experiences/items").get.operation.parameters.map(_.name) should be(
-      Seq("organization", "number", "status", "experience", "country", "ip", "currency", "language", "context", "limit", "offset", "sort")
+    multi.findOperation("GET", "/users").get.operation.parameters.map(_.name) must be(
+      Seq("id")
     )
   }
-
-  it("validate") {
+/*
+  "validate" in {
     // path from flow api
     multi.upcastOperationBody(
       "POST",
       "/:organization/webhooks",
       Json.obj("url" -> "https://test.flow.io")
-    ) should equal(
+    ) must equal(
       Left(Seq("Missing required field for webhook_form: events"))
     )
 
@@ -58,12 +89,12 @@ class MultiServiceImplSpec extends AnyFunSpec with Matchers with Helpers {
       "POST",
       "/:orgKey",
       Json.obj("url" -> "https://test.flow.io")
-    ) should equal(
+    ) must equal(
       Left(Seq("Missing required fields for application_form: name, visibility"))
     )
   }
 
-  it("error on missing array indices") {
+  "error on missing array indices" in {
     // comes from decoding customer[address][streets][]=33b Bay St
     multi.upcastOperationBody(
       "PUT",
@@ -96,12 +127,12 @@ class MultiServiceImplSpec extends AnyFunSpec with Matchers with Helpers {
           |    }
           |}
         """.stripMargin)
-    ) should equal(
+    ) must equal(
       Left(Seq("order_put_form.customer.address.streets of type '[string]': element in position[0] must be a string and not an object"))
     )
   }
 
-  it("url query example") {
+  "url query example" in {
     val form = FormData.parseEncoded("name=John%20Doe&expiration_month=12&expiration_year=2017&cvv=123&cipher=VnHzBw%2BbaGrKZL0fimklhKupHJeowxK2Mqa9LbECCnb3R%2FxIgS1vr0sFg2mUGsXR7bsNV61UURB91VrWr19V1g%3D%3D&challenge%5Btext%5D=Flow&challenge%5Bcipher%5D=df2BQZykhnTfIVIX6Vg9yjUmyEprz3dLmUYU0O8GeyCZ0t3pn1nXSP7DRDfsZAASwtNupqyYx3G4W%2BmGlWQreg%3D%3D&callback=__flowjsonp0&method=post")
 
     val js = multi.upcastOperationBody(
@@ -110,13 +141,13 @@ class MultiServiceImplSpec extends AnyFunSpec with Matchers with Helpers {
       Json.toJson(form)
     ).toOption.get
 
-    (js \ "name").as[JsString].value should equal("John Doe")
-    (js \ "expiration_month").as[JsNumber].value should equal(12)
-    (js \ "expiration_year").as[JsNumber].value should equal(2017)
-    (js \ "cvv").as[JsString].value should equal("123")
+    (js \ "name").as[JsString].value must equal("John Doe")
+    (js \ "expiration_month").as[JsNumber].value must equal(12)
+    (js \ "expiration_year").as[JsNumber].value must equal(2017)
+    (js \ "cvv").as[JsString].value must equal("123")
   }
 
-  it("simple number example") {
+  "simple number example" in {
     val form = Json.obj(
       "url" -> 123,
       "events" -> 456
@@ -128,34 +159,34 @@ class MultiServiceImplSpec extends AnyFunSpec with Matchers with Helpers {
       Json.toJson(form)
     ).toOption.get
 
-    (js \ "url").as[JsString].value should equal("123")
-    (js \ "events").as[JsArray] should equal(JsArray(Seq(JsString("456"))))
+    (js \ "url").as[JsString].value must equal("123")
+    (js \ "events").as[JsArray] must equal(JsArray(Seq(JsString("456"))))
   }
 
-  it("offers validation error w/ verb replacement") {
+  "offers validation error w/ verb replacement" in {
     multi.upcastOperationBody(
       "OPTIONS",
       "/:organization/webhooks",
       Json.obj("url" -> "https://test.flow.io", "events" -> "*")
-    ) should equal(
+    ) must equal(
       Left(Seq(
         "HTTP method 'OPTIONS' not defined for path '/:organization/webhooks' - Available methods: GET, POST, PUT, DELETE"
       ))
     )
   }
 
-  it("validate union type discriminator") {
+  "validate union type discriminator" in {
     multi.upcastOperationBody(
       "POST",
       "/:organization/authorizations",
       Json.obj("discriminator" -> "authorization_form")
     ) match {
-      case Left(errors) => errors.head.contains("Invalid discriminator 'authorization_form'") should be(true)
+      case Left(errors) => errors.head.contains("Invalid discriminator 'authorization_form'") must be(true)
       case Right(_) => sys.error("Expected error")
     }
   }
 
-  it("validate union type") {
+  "validate union type" in {
     multi.upcastOperationBody(
       "POST",
       "/:organization/authorizations",
@@ -163,9 +194,9 @@ class MultiServiceImplSpec extends AnyFunSpec with Matchers with Helpers {
         "order_number" -> "123",
         "discriminator" -> "merchant_of_record_authorization_form"
       )
-    ) should equal(
+    ) must equal(
       Left(Seq("Missing required field for merchant_of_record_authorization_form: token"))
     )
   }
-
+*/
 }
